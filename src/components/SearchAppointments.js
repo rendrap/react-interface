a914df4b5dcd40a5dec7f162acab2d2988e5e88e
rendrap <rendrapradana@gmail.com>
2021-02-22 08:00:10 +0700

    delete apt

---
+++ src/components/SearchAppointments.js
@@ -0,0 +1,9 @@
+import React, { Component } from 'react';
+
+class SearchApointments extends Component {
+    render() {
+        return <div>Search Appointments</div>;
+    }
+}
+
+export default SearchApointments;
